7a8c6ed61f782f579b3e1a0c61d7924fd88d9d7a
Dell-User <Dell-User@DESKTOP-UI1ISCL>
2023-10-03 13:14:04 +0200

    Add project files.

---
+++ 2Arithmetic Operators4/Form1.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        btnAdd = New Button()
+        btnSubtract = New Button()
+        btnDivision = New Button()
+        btnMultiplication = New Button()
+        Label3 = New Label()
+        TextBox3 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(106, 49)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(41, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Label1"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(106, 96)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(41, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Label2"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(222, 41)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(222, 96)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 3
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Location = New Point(48, 287)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(75, 23)
+        btnAdd.TabIndex = 4
+        btnAdd.Text = "+"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btnSubtract
+        ' 
+        btnSubtract.Location = New Point(190, 287)
+        btnSubtract.Name = "btnSubtract"
+        btnSubtract.Size = New Size(75, 23)
+        btnSubtract.TabIndex = 5
+        btnSubtract.Text = "-"
+        btnSubtract.UseVisualStyleBackColor = True
+        ' 
+        ' btnDivision
+        ' 
+        btnDivision.Location = New Point(308, 287)
+        btnDivision.Name = "btnDivision"
+        btnDivision.Size = New Size(75, 23)
+        btnDivision.TabIndex = 6
+        btnDivision.Text = "/"
+        btnDivision.UseVisualStyleBackColor = True
+        ' 
+        ' btnMultiplication
+        ' 
+        btnMultiplication.Location = New Point(439, 287)
+        btnMultiplication.Name = "btnMultiplication"
+        btnMultiplication.Size = New Size(75, 23)
+        btnMultiplication.TabIndex = 7
+        btnMultiplication.Text = "*"
+        btnMultiplication.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(106, 138)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(51, 15)
+        Label3.TabIndex = 8
+        Label3.Text = "Answers"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(222, 138)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 9
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox3)
+        Controls.Add(Label3)
+        Controls.Add(btnMultiplication)
+        Controls.Add(btnDivision)
+        Controls.Add(btnSubtract)
+        Controls.Add(btnAdd)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btnDivision As Button
+    Friend WithEvents btnMultiplication As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+End Class
---
+++ 2Arithmetic Operators4/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        ' MsgBox("The sum is " & Val(TextBox1.Text) + Val(TextBox2.Text))
+        ' Label3.Text = Val(TextBox1.Text) + Val(TextBox2.Text)
+        TextBox3.Text = Val(TextBox1.Text) + Val(TextBox2.Text)
+
+    End Sub
+
+    Private Sub btnSubtract_Click(sender As Object, e As EventArgs) Handles btnSubtract.Click
+        ' MsgBox("The subtract is " & Val(TextBox1.Text) - Val(TextBox2.Text))
+        TextBox3.Text = Val(TextBox1.Text) - Val(TextBox2.Text)
+    End Sub
+
+    Private Sub btnDivision_Click(sender As Object, e As EventArgs) Handles btnDivision.Click
+        'MsgBox("The division is " & Val(TextBox1.Text) / Val(TextBox2.Text))
+        TextBox3.Text = Val(TextBox1.Text) / Val(TextBox2.Text)
+    End Sub
+
+    Private Sub btnMultiplication_Click(sender As Object, e As EventArgs) Handles btnMultiplication.Click
+        'MsgBox("The multiplication is " & Val(TextBox1.Text) * Val(TextBox2.Text))
+        TextBox3.Text = Val(TextBox1.Text) * Val(TextBox2.Text)
+    End Sub
+End Class
